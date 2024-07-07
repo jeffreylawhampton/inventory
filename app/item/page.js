ae5edf776c465228683d419d9242cfc8c13689b1
jeffreylawhampton <jeffreylawhampton@gmail.com>
2024-07-07 17:38:31 -0400

    added auth0, filestack, fontawesome, dnd

---
+++ app/item/page.js
@@ -0,0 +1,13 @@
+import FilePicker from "../components/FilePicker";
+const Item = () => {
+  return (
+    <div>
+      Item
+      <div className="h-fit">
+        <FilePicker />
+      </div>
+    </div>
+  );
+};
+
+export default Item;
