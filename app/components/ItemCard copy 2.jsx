547c0ac9ed6b7de4071e091b8e6fb6d585cdbe5d
jeffreylawhampton <jeffreylawhampton@gmail.com>
2024-11-15 22:14:01 -0500

    hiding divs test

--- app/components/ItemCard copy 2.jsx
+++ app/components/ItemCard copy 2.jsx
@@ -7,10 +7,10 @@ import DetailsSpoiler from "./DetailsSpoiler";
 import DetailsTrigger from "./DetailsTrigger";
 
 const ItemCard = ({ item, showFavorite = true, handleFavoriteClick }) => {
-  // const [showDetails, setShowDetails] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
   return (
-    <div className="rounded-md overflow-hidden relative  bg-bluegray-200">
+    <div className="rounded-md overflow-hidden relative drop-shadow-md bg-bluegray-200 hover:bg-bluegray-300 active:bg-bluegray-300 active:drop-shadow-sm">
       <Link
         href={`/items/${item.id}`}
         className="w-full h-full absolute top-0 left-0"
@@ -29,12 +29,12 @@ const ItemCard = ({ item, showFavorite = true, handleFavoriteClick }) => {
           <h1 className="text-lg font-semibold leading-tight break-words hyphens-auto text-pretty">
             {item?.name}
           </h1>{" "}
-          {/* {showFavorite ? (
+          {showFavorite ? (
             <Favorite onClick={handleFavoriteClick} item={item} position="" />
-          ) : null} */}
+          ) : null}
         </span>
 
-        {/* <DetailsSpoiler
+        <DetailsSpoiler
           item={item}
           showDetails={showDetails}
           showOuterCategories
@@ -44,7 +44,7 @@ const ItemCard = ({ item, showFavorite = true, handleFavoriteClick }) => {
         <DetailsTrigger
           setShowDetails={setShowDetails}
           showDetails={showDetails}
-        /> */}
+        />
       </div>
     </div>
   );
