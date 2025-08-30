01c9a2535a47e596f4212f586699e6d791acbacf
jeffreylawhampton <jeffreylawhampton@gmail.com>
2025-08-30 14:33:37 -0400

    testing menu

--- app/components/CategoryPopup copy.jsx
+++ app/components/CategoryPopup copy.jsx
@@ -1,17 +1,16 @@
-import { Menu } from "@mantine/core";
+import { Popover } from "@mantine/core";
 import { CategoryPill } from ".";
 import { useContext } from "react";
 import { ModalContext } from "../providers";
 import { Tag } from "lucide-react";
 
 export default function CategoryPopup({ item }) {
-  const disabled = !item?.categories?.length;
   const { activePopoverId, setActivePopoverId } = useContext(ModalContext);
   const opened = activePopoverId === item.id + "-tags";
   const setOpened = () => setActivePopoverId(opened ? null : item.id + "-tags");
 
   return (
-    <Menu
+    <Popover
       position="top"
       classNames={{
         dropdown: "w-max !max-w-[95vw] !py-1.5 !px-2.5",
@@ -25,14 +24,11 @@ export default function CategoryPopup({ item }) {
       closeOnClickOutside
       opened={opened}
       onChange={setOpened}
-      trigger="click-hover"
-      disabled={disabled}
     >
-      <Menu.Target>
+      <Popover.Target>
         <button
-          disabled={disabled}
-          onClick={disabled ? null : setOpened}
-          className="disabled:opacity-50 relative rounded h-full min-h-[32px] px-2 flex !items-center text-center [&>svg]:fill-primary-700 hover:brightness-75"
+          onClick={item?.categories?.length ? setOpened : null}
+          className="relative rounded h-full min-h-[32px] px-2 flex !items-center text-center [&>svg]:fill-primary-700 hover:brightness-75"
         >
           <Tag
             size={25}
@@ -41,14 +37,14 @@ export default function CategoryPopup({ item }) {
             className="relative left-1"
           />
         </button>
-      </Menu.Target>
-      <Menu.Dropdown>
+      </Popover.Target>
+      <Popover.Dropdown>
         <div className="flex flex-wrap-none gap-1 items-center">
           {item?.categories?.map((category) => (
             <CategoryPill category={category} key={category.name} showTag />
           ))}
         </div>
-      </Menu.Dropdown>
-    </Menu>
+      </Popover.Dropdown>
+    </Popover>
   );
 }
